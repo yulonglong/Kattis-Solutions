0d92a79d2e887e33847db15135a37a025c13caf3
Steven Kester Yuwono <sky@u.nus.edu>
2018-12-19 14:12:28 +0800

    Solved princeandprincess

---
+++ princeandprincess.cpp
@@ -0,0 +1,45 @@
+// https://open.kattis.com/problems/princeandprincess
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 251
+#define INF 2e9
+typedef pair<int,int> ii;
+
+int memo[MAX*MAX];
+int ar[MAX*MAX];
+
+int main() {
+	int t; scanf("%d",&t);
+	for (int tc=1;tc<=t;tc++) {
+		memset(memo,0,sizeof(memo));
+		memset(ar,-1,sizeof(ar));
+		int n,p,q;	scanf("%d %d %d",&n,&p,&q);
+		for (int i=0;i<=p;i++) {
+			int val; scanf("%d",&val);
+			ar[val] = i;
+		}
+		vector<int> seq;
+		for (int i=0;i<=q;i++) {
+			int val; scanf("%d",&val);
+			// If this item exists in A, record the index sequence
+			if (ar[val] != -1)
+				seq.push_back(ar[val]);
+		}
+		
+		// LIS (Longest Increasing Subsequence) 
+		// N log N
+		vector<int> ans; // Keep the sequence of possible answers here
+		ans.assign(seq.size(),INF);
+		int ansMax = 0;
+		for (int idx : seq) {
+			// perform binary search (log N) and optimize longest sequence
+			int ansIdx = lower_bound(ans.begin(), ans.end(), idx) - ans.begin(); 
+			ans[ansIdx] = idx;
+			ansMax = max(ansMax, ansIdx);
+		}
+		
+		printf("Case %d: %d\n", tc, ansMax+1);
+	}
+	return 0;
+}
